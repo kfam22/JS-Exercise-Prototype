7aefed7593a8ef17002fdf58f4b41d565331086e
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2021-11-10 18:17:20 -0500

    completed tasks 1-2

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
         + If a plane lands, its `isFlying` property is set to false.
 */
 
+const { throwStatement } = require("@babel/types");
+
 // EXAMPLE SOLUTION CODE:
 function Airplane(name) {
   this.name = name;
@@ -39,14 +41,42 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
+const kyler = new Person('Kyler', 25);
+const ben = new Person ('Bennet', 28);
 
+// console.log(kyler.toString());
+// console.log(ben.toString());
+
+// ben.eat('pizza');
+// ben.eat('taco');
+// ben.eat('sushi');
+// ben.eat('ramen');
+// ben.eat('sandwhich');
+// ben.eat('cake');
+
+// console.log(ben.stomach);
+// ben.poop();
+// console.log(ben.stomach);
 
 
 /*
@@ -56,16 +86,26 @@ function Person() {
         + should initialize with an `tank` at 0
         + should initialize with an `odometer` at 0
     - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
-    - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
+    - ***STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
         + Should cause the the `tank` to go down taking `milesPerGallon` into account.
-    - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
+    - ***STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function(gallons){
+  return this.tank += gallons
+}
+
+const carOne = new Car('Mustang', 15);
+console.log(carOne.fill(45));
+console.log(carOne.fill(10));
 
 
 /*
